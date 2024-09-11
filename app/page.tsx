27c75f53dd6a371116eb3fed3d4a6613d897585d
raymond <raymond.luke.spartz@gmail.com>
2024-09-11 09:36:54 -0500

    started on ryver section

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,36 @@
-import { HeroSection } from "@/components/Hero/HeroSection";
-import { PricingSection } from "@/components/PricingSection";
+import { HeroSection } from '@/components/Hero/HeroSection';
+import { PricingSection } from '@/components/PricingSection';
+import { RyverSection } from '@/components/RyverSection/RyverSection';
 import {
-	getProducts,
-	getSubscription,
-	getUser,
-} from "@/utils/supabase/queries";
-import { createClient } from "@/utils/supabase/server";
-import { FAQSection } from "@/components/FAQSection";
-import { ServiceSection } from "@/components/ServiceSection";
+  getProducts,
+  getSubscription,
+  getUser
+} from '@/utils/supabase/queries';
+import { createClient } from '@/utils/supabase/server';
+import { FAQSection } from '@/components/FAQSection';
+import { ServiceSection } from '@/components/ServiceSection';
 
 export default async function PricingPage() {
-	const supabase = createClient();
-	const [user, products, subscription] = await Promise.all([
-		getUser(supabase),
-		getProducts(supabase),
-		getSubscription(supabase),
-	]);
+  const supabase = createClient();
+  const [user, products, subscription] = await Promise.all([
+    getUser(supabase),
+    getProducts(supabase),
+    getSubscription(supabase)
+  ]);
 
-	return (
-		<div>
-			<HeroSection />
-			<FAQSection />
-			<ServiceSection />
-			<PricingSection
-				user={user}
-				products={products ?? []}
-				subscription={subscription}
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <HeroSection />
+      <FAQSection />
+      <ServiceSection />
+      {/* <PricingSection
+        user={user}
+        products={products ?? []}
+        subscription={subscription}
+      /> */}
+      <div className="h-screen"></div>
+      <RyverSection />
+      <div className="h-screen"></div>
+    </div>
+  );
 }
