9cd2eb1e81583bab7f1ec3a394558d5d3389c3f8
numbercutter <jamlets@protonmail.com>
2024-09-14 18:52:09 -0400

    gtag and mclarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,14 @@ import { Toaster } from "@/components/ui/Toasts/toaster";
 import { getURL } from "@/utils/helpers";
 import type { Metadata } from "next";
 import { type PropsWithChildren, Suspense } from "react";
+import Script from "next/script";
 import "styles/main.css";
 
 const title = "Next.js Subscription Starter";
 const description = "Brought to you by Vercel, Stripe, and Supabase.";
+
+const clarityTrackingCode = process.env.NEXT_PUBLIC_CLARITY_TRACKING_CODE;
+const gtagId = process.env.NEXT_PUBLIC_GTAG_ID;
 
 export const metadata: Metadata = {
 	metadataBase: new URL(getURL()),
@@ -34,6 +38,34 @@ export default async function RootLayout({ children }: PropsWithChildren) {
 				<Suspense>
 					<Toaster />
 				</Suspense>
+				{clarityTrackingCode && (
+					<Script id="microsoft-clarity" strategy="afterInteractive">
+						{`
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "${clarityTrackingCode}");
+						`}
+					</Script>
+				)}
+				{gtagId && (
+					<>
+						<Script
+							src={`https://www.googletagmanager.com/gtag/js?id=${gtagId}`}
+							strategy="afterInteractive"
+						/>
+						<Script id="google-analytics" strategy="afterInteractive">
+							{`
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+
+								gtag('config', '${gtagId}');
+							`}
+						</Script>
+					</>
+				)}
 			</body>
 		</html>
 	);
