90520671f025639a68376632cb1d09cac53e92cb
numbercutter <jamlets@protonmail.com>
2024-10-19 17:59:34 -0400

    remove id

--- app/(user)/signin/page.tsx
+++ app/(user)/signin/page.tsx
@@ -1,8 +1,28 @@
-import { getDefaultSignInView } from '@/utils/auth-helpers/settings';
+import OauthSignIn from '@/components/AuthForms/OauthSignIn';
+import Card from '@/components/Card';
+import { getAuthTypes } from '@/utils/auth-helpers/settings';
+import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
-export default function SignIn() {
-  const defaultView = getDefaultSignInView();
+export default async function SignIn() {
+  const { allowOauth } = getAuthTypes();
 
-  return redirect(`/signin/${defaultView}`);
+  // Check if the user is already logged in and redirect to the account page if so
+  const supabase = createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect('/');
+  }
+
+  return (
+    <div className="height-screen-helper flex justify-center">
+      <div className="m-auto flex w-80 max-w-lg flex-col justify-between p-3">
+        <Card title="Sign In">{allowOauth && <OauthSignIn />}</Card>
+      </div>
+    </div>
+  );
 }
