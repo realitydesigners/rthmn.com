15feadd554aeed4c11131398e51162ed29dc2d68
numbercutter <jamlets@protonmail.com>
2024-11-02 12:22:29 -0400

    updated middlware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
 import { type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
+import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  // Only check auth for /user routes
+  if (request.nextUrl.pathname.startsWith('/user')) {
+    return await updateSession(request);
+  }
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    // Match all routes within the @(user) folder
-    '/(user)/(.*)', // This will match all routes under the @(user) folder
-    /*
-     * Exclude static files and images
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+    // Match all routes within the user folder
+    '/user/:path*'
   ]
 };
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -33,11 +33,7 @@ export async function updateSession(request: NextRequest) {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/signin') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
+  if (!user) {
     const url = request.nextUrl.clone();
     url.pathname = '/signin';
     return NextResponse.redirect(url);
