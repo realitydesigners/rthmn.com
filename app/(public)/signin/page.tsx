da00e878a790240fd49eeb12d7dd2bf246ce66e6
Mitch <jamlets@protonmail.com>
2024-12-05 09:34:37 -0500

    beta start

--- app/(public)/signin/page.tsx
+++ app/(public)/signin/page.tsx
@@ -80,34 +80,8 @@ export default function SignIn() {
               </form>
             </div>
           )}
-          <div className="mt-8">
-            <SocialMediaLinks />
-          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-const socialMediaLinks: { icon: IconType; href: string }[] = [
-  { icon: FaDiscord, href: '#' },
-  { icon: FaInstagram, href: '#' },
-  { icon: FaTwitter, href: '#' },
-  { icon: FaYoutube, href: '#' }
-];
-
-function SocialMediaLinks() {
-  return (
-    <div className="mt-12 flex justify-center space-x-8">
-      {socialMediaLinks.map((social, index) => (
-        <Link
-          key={index}
-          href={social.href}
-          className="text-gray-400 transition-colors duration-200 hover:text-white"
-        >
-          <social.icon size={32} />
-        </Link>
-      ))}
     </div>
   );
 }
