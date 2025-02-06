e37b2f6e046c81322d3722886602d23249efa2a3
Raymond <raymond.luke.spartz@gmail.com>
2025-02-06 08:48:25 -0800

    admin fix

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,11 +1,11 @@
 import { redirect } from 'next/navigation';
 import { AdminNavbar } from '@/app/(admin)/_components/AdminNavbar';
 import DashboardProvider from '@/providers/DashboardProvider/client';
-import { QueryProvider } from '@/providers/QueryProvider';
 import { WebSocketProvider } from '@/providers/WebsocketProvider';
 import { getUser } from '@/utils/supabase/queries';
 import { createClient } from '@/utils/supabase/server';
 import { AUTHORIZED_IDS } from './tokens';
+import UserProvider from '@/providers/UserProvider';
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
     const supabase = await createClient();
@@ -16,15 +16,15 @@ export default async function AdminLayout({ children }: { children: React.ReactN
     }
 
     return (
-        <QueryProvider>
-            <WebSocketProvider>
+        <WebSocketProvider>
+            <UserProvider>
                 <DashboardProvider>
                     <div id='app-container' className='relative min-h-screen overflow-y-auto'>
                         <AdminNavbar />
                         <main className='w-full bg-black transition-all duration-300 ease-in-out'>{children}</main>
                     </div>
                 </DashboardProvider>
-            </WebSocketProvider>
-        </QueryProvider>
+            </UserProvider>
+        </WebSocketProvider>
     );
 }
